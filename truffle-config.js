1680536100541f908d82f515d441a1de78b84104
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2022-02-10 20:37:59 +0530

    loading contract

--- truffle-config.js
+++ truffle-config.js
@@ -33,7 +33,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
+  contracts_build_directory: "./public/contracts",
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
@@ -41,7 +41,7 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    contract_build_directory: "/public/contracts",
+
     development: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 7545, // Standard Ethereum port (default: none)
