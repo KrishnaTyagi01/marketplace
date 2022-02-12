78fa8f136288da1cf0a214c1125ba5bde874fb2d
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2022-02-12 23:29:14 +0530

    hooks changed

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,11 +1,18 @@
+import { useAccount, useOwnedCourses } from "@components/hooks/web3";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 
-export default function OwnedCourses() {
+import { getAllCourses } from "@content/courses/fetcher";
+
+export default function OwnedCourses({ courses }) {
+  const { account } = useAccount();
+  const { ownedCourses } = useOwnedCourses(courses, account.data);
+
   return (
     <>
+      {JSON.stringify(ownedCourses.data)}
       <div className="py-4">
         <MarketHeader />
       </div>
@@ -17,4 +24,13 @@ export default function OwnedCourses() {
       </section>
     </>
   );
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses();
+  return {
+    props: {
+      courses: data,
+    },
+  };
 }
